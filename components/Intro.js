13836859583883c204a62301e9dcf00f84105d04
Mohamed K <mohamedkore@gmail.com>
2021-02-06 17:36:15 -0500

    Update Intro.js

--- components/Intro.js
+++ components/Intro.js
@@ -8,7 +8,7 @@ function Intro() {
                 <div className="large-header text-center flex justify-center align-middle">
                     <h1 className="font-thin uppercase m-auto text-xl sm:text-4xl text-gray-100">
                         <animated.span ><p>Hi, I'm <span className="font-bold text-red-500">Mohamed.</span></p></animated.span>
-                        <animated.span  className=""><p>I'm a Software Developer.</p></animated.span>
+                        <animated.span  className=""><p>I'm an aspiring  Software/DevOps Developer.</p></animated.span>
                         <animated.br />
                         <animated.span className="flex flex-wrap items-center text-sm justify-center w-full mt-8">
                             <a className="" href="#projects"><button className="btn mr-4 hover:bg-teal-600 focus:border-green-900 focus:border-4 hover:border-transparent">See My Work</button></a>
